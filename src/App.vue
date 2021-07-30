300f5ae3bbc46dc062b4741ad3b3045d8558d830
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-07-30 12:56:13 -0300

    First part of the project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+  components: { Calculator }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Bold.ttf");
+}
+
+*{
+  font-family: "RobotoMono", monospace;
+}
+
+body{
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(80,38,153,1) 39%, rgba(102,0,255,1) 96%);
+  
+}
+</style>
---
+++ src/main/Calculator.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="calculator">
+      <Display value="1000"/>
+      <Button label="AC" triple @onCalc="clearMemory"/>
+      <Button label="/" operation @onCalc="setOperation"/>
+      <Button label="7" @onCalc="addDigit"/>
+      <Button label="8" @onCalc="addDigit"/>
+      <Button label="9" @onCalc="addDigit"/>
+      <Button label="*" operation @onCalc="setOperation"/>
+      <Button label="4" @onCalc="addDigit"/>
+      <Button label="5" @onCalc="addDigit"/>
+      <Button label="6" @onCalc="addDigit"/>
+      <Button label="-" operation @onCalc="setOperation"/>
+      <Button label="1" @onCalc="addDigit"/>
+      <Button label="2" @onCalc="addDigit"/>
+      <Button label="3" @onCalc="addDigit"/>
+      <Button label="+" operation @onCalc="setOperation"/>
+      <Button label="0" double @onCalc="addDigit"/>
+      <Button label="." @onCalc="addDigit"/>
+      <Button label="=" operation @onCalc="setOperation"/>
+      
+     
+  </div>
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+
+export default {
+components: { Button, Display },
+methods: {
+  clearMemory(){
+    console.log('Limpar Memória!')
+  },
+  setOperation(operation) {
+    console.log('Operação ' + operation)
+  },
+  addDigit(n) {
+    console.log('Digito ' + n)
+  },
+}
+}
+</script>
+
+<style>
+.calculator {
+    width: 235px;
+    height: 320px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+
+</style>
